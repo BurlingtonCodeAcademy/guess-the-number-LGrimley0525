3f6fdf07691e8867c130630877c695bf7d450c67
LGrimley0525 <60547230+LGrimley0525@users.noreply.github.com>
2020-02-08 14:44:02 -0500

    to fix looping issue

--- index.js
+++ index.js
@@ -1,117 +1,46 @@
 const readline = require('readline');
+
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    rl.question(questionText, resolve);
-  });
+//listing of functions below
+function ask(questionText) {//allows the program to ask and receive
+    return new Promise((resolve, reject) => {
+        rl.question(questionText, resolve);
+    });
+}
+
+function randomInt(min, max) {//allows the random number generated within specified range 
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min
 }
 
 start();
 
 
-
 async function start() {
-  // this should start the program 
-  console.log("Let's play a game where you pick a number between 1 and 100 and then I will try to guess it in as few tries as possible! \n\n");
-  let guess = randomInt(1, 100);
-  let numQuestion = (await ask('Is it... ' + guess + '? Y/N?  ')).toString().toLowerCase().trim(); //asking if this is the number and their response
-  let lowNumber = randomInt(1, ((guess) - 1))
-  let highNumber = randomInt(((guess) + 1), 100)
-  if (numQuestion === 'y') {
-    console.log("Your number is " + guess + "!\nDamn I am good!")
-    process.exit()
-  }
-  else {  //this loop is if the computer guess is wrong
-    let newQuestion = (await ask('Hmmm is your number higher or lower than my guess?  Type H or L  ')).toString().toLowerCase().trim()
+    console.log("Let's play a game where you pick a number between 1 and 100 and then I will try to guess it in as few tries as possible! \n\n");//this gets the party started 
+    let minValue = 1;
+    let maxValue = 100;
+    let ranNum = randomInt(minValue, maxValue)  //randomly generated number between min and max values 
+    let guess = (await ask("Is your number " + ranNum + " ?  Y/N ")).toString().toLowerCase().trim()  //asks user if number is right and converts answer to lower case for easeand removes white space 
 
-    if (newQuestion === 'h') {
-      while (newQuestion === 'h') {
-        let highQuest = (await ask("Is it ..." + highNumber + "?  Y/N  ")).toString().toLowerCase().trim()
-        console.log(" " + highQuest + " ")
+    while (guess !== "y") { //this is run when user responds with a ‘n’ 
+        let newQuestion = (await ask("Hmmm is your number higher or lower than my guess?  H/L")).toString().toLowerCase().trim()//asks user if their guess is higher or lower to allow program next step and then converts answer to lower case and removes any white space 
+        if (newQuestion === "h") { //if user number is higher, this will generate a new random number that is higher than their guess but less than = to 100 
+            minValue = ranNum
+            ranNum = randomInt(minValue, maxValue)
+            guess = (await ask("Is your number.. " + ranNum + " ? Y/N")).toString().toLowerCase().trim()
 
-        if (highQuest === 'n') {
-          let newQuest = (await ask('Hmmm is your number higher or lower than my guess?  Type H or L  ')).toString().toLowerCase().trim()
-          console.log(" " + newQuest + " ")
-          if (newQuest === 'h') {
-            while (newQuestion === 'h') {
-              let highNumber = randomInt(((guess) + 1), 100)
-              let highQuest = (await ask("Is it ..." + highNumber + "?  Y/N  ")).toString().toLowerCase().trim()
-              console.log(" " + highQuest + " ")
-              if (highQuest === 'n') {
-                let highNumber = randomInt(((guess) + 1), 100)
-                let highQuest = (await ask("Is it ..." + highNumber + "?  Y/N  ")).toString().toLowerCase().trim()
-                console.log(" " + highQuest + " ")
-              }
-              else {
-                console.log("Your number is " + highNumber + "! \nDamn I am good")
-                process.exit()
-              }
-            }
-            console.log("Your number is " + highNumber + "!\nDamn I am good!")
-            process.exit()
-
-          }
         }
-        else {
-          console.log(" Your number is " + highNumber + "! \nDamn I am good")
-          process.exit()
+        else {//if user number is lower, this will generate a new random number between 1 and the guess -1 
+            maxValue = ranNum
+            ranNum = randomInt(minValue, maxValue)
+            guess = (await ask("Is your number... " + ranNum + " ?  Y/N")).toString().toLowerCase().trim()
         }
-      }
+
     }
-    else {
-      
-        while (newQuestion === 'l') {
-          let lowQuest = (await ask("Is it ..." + lowNumber + "?  Y/N  ")).toString().toLowerCase().trim()
-          console.log(" " + lowQuest + " ")
-  
-          if (lowQuest === 'n') {
-            let newQuest = (await ask('Hmmm is your number higher or lower than my guess?  Type H or L  ')).toString().toLowerCase().trim()
-            console.log(" " + newQuest + " ")
-            if (newQuest === 'l') {
-              while (newQuestion === 'l') {
-                let lowNumber = randomInt(1, ((guess) - 1))
-                let lowQuest = (await ask("Is it ..." + lowNumber + "?  Y/N  ")).toString().toLowerCase().trim()
-                console.log(" " + lowQuest + " ")
-                if (lowQuest === 'n') {
-                  while (lowQuest === 'n') {
-                    let lowNumber = randomInt(1, ((guess) - 1))
-                  let lowQuest = (await ask("Is it ..." + lowNumber + "?  Y/N  ")).toString().toLowerCase().trim()
-                  console.log(" " + lowQuest + " ")
-                }}
-                else {
-                  console.log("Your number is " + lowNumber + "! \nDamn I am good")
-                  process.exit()
-                }
-              }
-              console.log("Your number is " + lowNumber + "!\nDamn I am good!")
-              process.exit()
-  
-            }
-          }
-          else {
-            console.log(" Your number is " + lowNumber + "! \nDamn I am good")
-            process.exit()
-          }
-        }
-      }
-     
 
-
-  console.log("Your number is " + guess + "!\nDamn I am good!")
-  process.exit()
-
-}
-
-
-
-
-
-
-
-function randomInt(min, max) {
-  let range = max - min + 1;
-  return min + Math.floor(Math.random() * range)
-  //allows the random number generated within game range 
-}
-}
+    console.log("Your number is " + ranNum + "! \nDamn I am good!")
+    process.exit()
+} 
